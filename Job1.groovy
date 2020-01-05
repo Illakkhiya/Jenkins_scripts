e900a46c17d4f9bb1fed7710c57576c07484c9e4
Illakkhiya <illakkhiya@gmail.com>
2020-01-04 19:47:14 -0800

    mvn build

--- Job1.groovy
+++ Job1.groovy
@@ -1,6 +1,11 @@
 node {
     stage('Test') {
 	echo 'Hello World'
-	build job: 'Job2', quietPeriod: 300, wait: false
+        steps {
+		git branch: 'master', url: 'https://github.com/Illakkhiya/Java_maven'
+		sh "mvn clean packge -q -DskipTests=true"
+		sh "mvn test"
+	}
+	//build job: 'Job2', quietPeriod: 300, wait: false
     }
 }
