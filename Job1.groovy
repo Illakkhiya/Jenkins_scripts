c68ce4ccf3aee31ae7ea40eb1c5c7082241100cb
Illakkhiya <illakkhiya@gmail.com>
2020-01-04 20:30:35 -0800

    fix mvn target

--- Job1.groovy
+++ Job1.groovy
@@ -2,7 +2,7 @@ node {
     stage('Test') {
 	echo 'Hello World'
 	git branch: 'master', url: 'https://github.com/Illakkhiya/Java_maven'
-	sh "mvn clean packge -q -DskipTests=true"
+	sh "mvn clean package -q -DskipTests=true"
 	sh "mvn test"
     }
 }
