2110c72575cec9d5685c9e610236603d7644b30d
Illakkhiya <illakkhiya@gmail.com>
2020-01-04 19:50:31 -0800

    remove steps

--- Job1.groovy
+++ Job1.groovy
@@ -1,11 +1,8 @@
 node {
     stage('Test') {
 	echo 'Hello World'
-        steps {
-		git branch: 'master', url: 'https://github.com/Illakkhiya/Java_maven'
-		sh "mvn clean packge -q -DskipTests=true"
-		sh "mvn test"
-	}
-	//build job: 'Job2', quietPeriod: 300, wait: false
+	git branch: 'master', url: 'https://github.com/Illakkhiya/Java_maven'
+	sh "mvn clean packge -q -DskipTests=true"
+	sh "mvn test"
     }
 }
